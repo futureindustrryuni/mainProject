ac8d97888f2cc55e37aa3dd10a46023364a9e223
Abtin Arian <scorkar@gmail.com>
2025-09-18 19:01:22 +0330

    Nothing important just fixed some bugs

--- backend/database/migrations/2025_09_14_090729_skills_table.php
+++ backend/database/migrations/2025_09_14_090729_skills_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('user_skills', function (Blueprint $table) {
 
             $table->id();
-            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
             $table->enum('skill', ['Next JS','C++','Figma','MySQL','Jquery','CSS','Python','CSS','React','PHP','Html','Javascript','Laravel']);
             $table->unsignedTinyInteger('percentage');
             $table->timestamps();
