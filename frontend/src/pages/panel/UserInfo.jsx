d2c26225ccd9c089ea616733dd6d6c5d7085d6ae
MohamadAminPour <ytornado37@gmail.com>
2025-09-21 17:20:34 +0430

    add some change to userInfo from front page

--- frontend/src/pages/panel/UserInfo.jsx
+++ frontend/src/pages/panel/UserInfo.jsx
@@ -142,7 +142,7 @@ export default function UserInfo() {
       <div className={`${isOpen ? "w-[80%]" : "w-[100%]"}`}>
         <TopBar isOpen={isOpen} setIsOpen={setIsOpen} />
         <div className="p-5 dark:text-white text-black">
-          <div className="flex flex-col bg-white dark:bg-[#1B202C] py-6 px-10 gap-7 rounded-xl shadow-[0_3px_15px_5px_rgba(0,0,0,0.1)]">
+          <div className="flex flex-col bg-white dark:bg-[#1B202C] py-6 px-10 gap-7 rounded-xl shadow-[0_11px_50px_1px_rgba(0,0,0,0.1)]">
             <div className="flex items-center gap-1">
               <LiaUserSolid className="size-7" />
               <p className=" font-IranYekanBold text-[1rem]">اطلاعات فردی</p>
