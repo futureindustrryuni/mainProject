b3bfb7d4e41a16f5f98275bcd236c1b20e41e444
MohamadAminPour <ytornado37@gmail.com>
2025-09-25 00:00:52 +0330

    complete myProjects page from front pages

---
+++ frontend/src/components/MyProjectItem.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { HiOutlineTrash } from "react-icons/hi";
+import { TbEditCircle } from "react-icons/tb";
+import JalaliDate from "./JalaliDate";
+
+export default function MyProjectItem({ ...item }) {
+  return (
+    <li className="relative flex items-start flex-col sm:items-start xmd:flex-row gap-4 py-4 border-b-1 border-zinc-200 sm:border-zinc-200/10 ">
+      <img
+        src="/images/project5.png"
+        alt="طراحی وبسایت رستوران"
+        className="h-[100px] w-[150px] xsm:w-[230px] xsm:h-[165px] rounded-xl object-cover"
+      />
+      <div className="flex items-start flex-col mr-1 gap-2 h-full">
+        <h2 className="font-bold text-[1rem] xsm:text-[1.2rem] md:text-[1.4rem]  dark:text-[#FFFFFF]  text-black">
+          {item.title}
+        </h2>
+        <p className="text-[#999999] font-medium text-[.8rem] lg:text-[1rem] md:text-[.9rem] line-clamp-2 w-[12rem] sm:w-[10rem] md:w-[15rem]  lg:w-[30rem] ">
+          {item.description}
+        </p>
+        <p className="text-[#777] font-medium text-[.7rem] lg:text-[.9rem] md:text-[.8rem]">
+          تاریخ انتشار : {<JalaliDate gregorianDate={item.created_at} />}
+        </p>
+        <div className="flex gap-1 md:gap-1 mt-2">
+          {item.technologies.split("-").map((tech) => (
+            <span key={tech} className="bg-zinc-200/70 dark:bg-[#323232] text-black dark:text-[#FFFFFF] font-medium  text-[.7rem] md:text-[.8rem] px-3 py-1 rounded-[8px]">
+              {tech}
+            </span>
+          ))}
+        </div>
+      </div>
+      <div className="absolute top-[1rem] left-[.5rem] mt-1 flex gap-2">
+        <button className="text-[#ff9911] cursor-pointer" title="ویرایش">
+          <TbEditCircle className="sm:text-[18px] lg:text-[1.4rem] md:text-[25px]" />
+        </button>
+        <button className="text-red-500 cursor-pointer" title="حذف">
+          <HiOutlineTrash className="sm:text-[18px] lg:text-[1.4rem] md:text-[25px] text" />
+        </button>
+      </div>
+    </li>
+  );
+}
